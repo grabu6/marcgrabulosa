50fa008bf8bf1689317ff0ff0bb6ed8232aa5eb6
grabu6 <101642033+grabu6@users.noreply.github.com>
2022-11-17 19:25:59 +0100

    Add files via upload

---
+++ UF3/validar.php
@@ -0,0 +1,24 @@
+<?php
+include('utils.php');
+
+
+$correuCorrecte=false;
+$consulta=$connexio->prepare("SELECT nom, email, contrasenya FROM comptes");
+$consulta-> execute();
+
+foreach($consulta as $row){
+    if($row['email']==$_POST['email'] && $row['contrasenya']==md5($_POST['password'])){
+                header("location:hola.php");
+    }else{
+        $correuCorrecte=true;
+    }
+}
+
+if($correuCorrecte==true){
+    include("index.php");
+    ?>
+    <h3>Dades incorrectes!</h3>
+    <?php
+}
+    
+    
