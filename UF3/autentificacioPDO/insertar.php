010b0c0fab7ccd0bd30398c9b6968d69c59b62a5
grabu6 <101642033+grabu6@users.noreply.github.com>
2022-11-18 17:31:28 +0100

    Add files via upload

---
+++ UF3/autentificacioPDO/insertar.php
@@ -0,0 +1,43 @@
+<?php
+include("utils.php");
+
+if(isset($_POST['register'])){
+    if(strlen($_POST["name"]) >= 1 && strlen($_POST["email"]) >= 1 && strlen($_POST["password"])>= 1){
+        $nom= trim($_POST['name']);
+        $correu= trim($_POST['email']);
+        $password=md5(trim($_POST['password']));
+        $consulta1=$connexio->prepare("SELECT nom, usuari, contrasenya FROM identificats");
+        $consulta1-> execute();
+        $consulta2=$connexio->prepare("INSERT INTO identificats(nom, usuari, contrasenya) VALUES (?,?,?)");
+
+        foreach($consulta1 as $row){
+
+            if($row['usuari']==$_POST['email']){
+                ?>
+                <h3>Error! Usuari ja introduit !!!</h3>
+                <?php
+            }else{
+                $consulta2->execute(array($nom,$correu,$password));
+            }
+        }
+
+
+
+        if($consulta2){
+            ?>
+            <h3>T'has inscrit correctament!</h3>
+
+            <?php
+        }else{
+            ?>
+            <h3>Error!</h3>
+            <?php
+        }
+
+     } else{
+        ?>
+        <h3>Completi els camps correctament!</h3>
+        <?php
+    }
+}
+?>
